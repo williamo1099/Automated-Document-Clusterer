ce8f7ac61bb47dd5bd7d5e90a8e28895196a13da
William Oktavianus <williamoktavianus1099@gmail.com>
2020-09-03 19:50:30 +0700

    Penambahan  menu File, fungsi save dan load index
    Menambahkan menu File : save index dan load index, disertai fungsinya.

--- run.py
+++ run.py
@@ -1,11 +1,30 @@
 
 import os
+import pickle
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from Document import Document
 from Indexer import Indexer
 from Clusterer import Clusterer
+
+def save_index(index):
+    if index is not None:
+        with open('index.pickle', 'wb') as handle:
+            pickle.dump(index, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        warning_popup = tk.Tk()
+        warning_popup.wm_title('Saving an index')
+        label = tk.Label(warning_popup, text='There is no index to save!')
+        label.pack(side='top', fill='x', pady=10)
+        ok_button = tk.Button(warning_popup, text='Ok', command=warning_popup.destroy)
+        ok_button.pack()
+        warning_popup.mainloop()
+
+def load_index():
+    with open('index.pickle', 'rb') as handle:
+        global index
+        index = pickle.load(handle)
 
 def cluster():
     path = 'Document/'
@@ -24,10 +43,12 @@ def cluster():
             corpus.append(doc_i)
             
         # Build an inverted index.
-        indexer = Indexer()
-        for i in range(0, len(corpus)):
-            indexer.index(corpus[i])
-        index = indexer.get_inverted_index(len(corpus))
+        global index
+        if index is None:
+            indexer = Indexer()
+            for i in range(0, len(corpus)):
+                indexer.index(corpus[i])
+            index = indexer.get_inverted_index(len(corpus))
         
         # Do document clustering.
         clusterer = Clusterer()
@@ -37,9 +58,19 @@ def cluster():
         canvas.draw()
         canvas.get_tk_widget().pack()
         
+index = None
+
 window = tk.Tk()
 window.title('Document Clustering')
 window.geometry("500x500")
+
+menu = tk.Menu(window)
+window.config(menu=menu)
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Save index", command=lambda: save_index(index))
+file_menu.add_command(label="Load index", command=load_index)
+
 cluster_button = tk.Button(master=window, text='Cluster', command=cluster)
 cluster_button.pack()
 window.mainloop()
