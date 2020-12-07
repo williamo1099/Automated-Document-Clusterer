ef850be2c99803ccee6ecd72c13fc1fd3dde4a8d
William Oktavianus <williamoktavianus1099@gmail.com>
2020-12-08 01:18:53 +0700

    Perubahan kelas package clustering
    Menambahkan kelas Dendrogram dan mempercantik kelas Clusterer.
    P.S. Seharusnya masih ERROR (kelas gui belum diubah untuk menangani perubahan kelas-kelas tersebut).

--- src/clustering/Clusterer.py
+++ src/clustering/Clusterer.py
@@ -1,186 +1,99 @@
 
+from clustering.Dendrogram import Dendrogram
+
 from scipy.cluster.hierarchy import linkage, dendrogram, cophenet
 import matplotlib.pyplot as plt
 
 class Clusterer:
     
-    def set_dendrogram_height(self, dend):
-        """
-        Method untuk menghitung tinggi maksimal dari dendrogram.
-        Tinggi maksimal dari dendrogram disimpan sebagai nilai atribut dendrogram_height.
-
-        Parameters
-        ----------
-        dend : dictionary
-            Dendrogram visualisasi hasil pengelompokan.
-
-        Returns
-        -------
-        None.
-
-        """
-        # Mengambil seluruh nilai dalam dcoord (menjadi flat array).
-        dcoord_flat_list = []
-        for item in dend['dcoord']:
-            dcoord_flat_list += item
-        # Mengambil nilai maksimal dari dcoord_flat_list.
-        self.dendrogram_height = max(dcoord_flat_list)
-    
-    def get_dendrogram_height(self):
-        """
-        Method untuk mendapatkan nilai atribut dendogram_height, yang merupakan tinggi maksimal dari dendrogram.
-
-        Returns
-        -------
-        dendrogram_height : float
-            Tinggi maksimal dari dendrogram.
-
-        """
-        return self.dendrogram_height
-    
-    def set_cluster(self, dend):
-        """
-        Method untuk mengelompokkan dokumen teks yang ada berdasarkan dendrogram.
-        Pengelompokan dilakukan berdasarkan warna dan hasilnya disimpan dalam atribut cluster_list.
-        Hasil pengelompokan ditulis sebagai {cluster1: [doc1, doc2], cluster2: [doc3]}.
-        Sumber : http://www.nxn.se/valent/extract-cluster-elements-by-color-in-python.
-
-        Parameters
-        ----------
-        dend : dictionary
-            Dendrogram visualisasi hasil pengelompokan.
-
-        Returns
-        -------
-        None.
-
-        """
-        self.cluster_list = {}
-        for c, pi in zip(dend['color_list'], dend['icoord']):
-            for leg in pi[1:3]:
-                i = (leg - 5) / 10
-                if abs(i - int(i)) <= 0:
-                    if c not in self.cluster_list:
-                        self.cluster_list[c] = []
-                    self.cluster_list[c].append(dend['ivl'][int(i)])
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.proximity_matrix = []
     
     def get_cluster(self):
         """
-        Method untuk mendapatkan nilai atribut cluster_list, yang merupakan hasil pengelompokan dokumen teks.
+        Get a list of objects of each clusters.
 
         Returns
         -------
-        cluster_list : dictionary
-            Hasil pengelompokan, yang merupaakan daftar cluster beserta dokumen teks anggotanya.
-            Ditulis sebagai {cluster1 : [doc1, doc2], cluster2 : [doc3]}.
+        dictionary
+            A list of objects of each clusters.
+            Written as {cluster1: [doc1, doc2], cluster2: [doc3], etc.}..
 
         """
-        return self.cluster_list
+        return self.dendrogram.extract_clusters_by_color()
     
-    def set_cophenet_coeff(self, proximity_matrix, linkage):
+    def get_cophenetic_coeff(self):
         """
-        Method untuk menghitung nilai koefisien cophenet dan disimpan sebagai nilai atribut cophenet_coeff.
-        Nilai koefisien cophenet digunakan sebagai evaluasi hasil pengelompokan.
+        Count the cophenetic coefficient correlation (CPCC) for the result get.
+        The CPCC value can be used for an internal evaluation of the clusters.
+
+        Returns
+        -------
+        c : float
+            The copehenetic coefficient correlation (CPCC).
+
+        """
+        c, d = cophenet(self.linked, self.distance_matrix)
+        return c
+    
+    def set_distance_matrix(self, index):
+        """
+        Build a 1-D condensed distance matrix consisting cosine distances between documents in corpus.
 
         Parameters
         ----------
-        proximity_matrix : list
-            Matriks jarak condensed 1D.
-        linkage : array
-            Matriks linkage yang menyimpan hasil pengelompokan dari fungsi linkage.
+        index : dictionary
+            An inverted index.
 
         Returns
         -------
         None.
 
         """
-        c, d = cophenet(linkage, proximity_matrix)
-        self.cophenet_coeff = c
-    
-    def get_cophenetcoeff(self):
-        """
-        Method untuk mendapatkan nilai atribut cophenet_coeff, yang merupakan nilai koefisien cophenet.
-
-        Returns
-        -------
-        cophenet_coeff : float
-            Nilai koefisien cophenet sebagai evaluasi hasil pengelompokan.
-
-        """
-        return self.cophenet_coeff
-    
-    def create_proximity_matrix(self, index, corpus):
-        """
-        Method untuk membangun matriks jarak untuk seluruh dokumen teks.
-
-        Parameters
-        ----------
-        index : dictionary
-            Inverted index.
-        corpus : list
-            Daftar dokumen teks.
-
-        Returns
-        -------
-        matrix : list
-            Matriks jarak condensed 1D.
-
-        """
-        # Menghitung representasi vektor masing-masing dokumen teks yang ada.
-        for doc_i in corpus:
-            doc_i.set_vector(index, len(corpus))
-        # Menghitung matriks jarak berdasarkan representasi vektor.
-        matrix = []
-        for i in range(0, len(corpus)):
-            doc_i = corpus[i]
-            for j in range(i + 1, len(corpus)):
-                doc_j = corpus[j]
+        # Set each documents' vector representation.
+        for doc_i in self.corpus:
+            doc_i.set_vector(index, len(self.corpus))
+            
+        # Build a 1-D condensed distance matrix.
+        for i in range(0, len(self.corpus)):
+            doc_i = self.corpus[i]
+            for j in range(i + 1, len(self.corpus)):
+                doc_j = self.corpus[j]
                 distance = doc_i.count_distance(doc_j)
-                matrix.append(distance)
-        return matrix
+                self.proximity_matrix.append(distance)
     
-    def cluster(self, index, corpus, method, cut_off=0):
+    def cluster(self, index, method, cut_off=0):
         """
-        Method untuk melakukan proses pengelompokan dengan algoritma hierarchical clustering.
-        Pendekatan pengelompokan yang digunakan adalah agglomerative.
+        Do the agglomerative hierarchical clustering process for the given documents in corpus.
 
         Parameters
         ----------
         index : dictionary
-            Inverted index.
-        corpus : list
-            Daftar dokumen teks.
+            An inverted index.
         method : string
-            Nama metode yang akan digunakan (single, complete, average).
+            The method used for calculating distances between two clusters.
+            Possible values for this parameter are single, complete and average.
         cut_off : float, optional
-            Ketinggian titik cut-off. Nilai default adalah 0.0.
+            The height of a cut-off point. The default is 0.
 
         Returns
         -------
-        fig : figure
-            Dendrogram visualisasi hasil pengelompokan.
+        figure
+            A figure of a dendrogram visualizing the result of clustering process.
 
         """
-        # Menghitung matriks jarak 1D.
-        proximity_matrix = self.create_proximity_matrix(index, corpus)
-        # Melakukan proses hierarchical clustering, dengan metode single-linkage.
-        linked = linkage(proximity_matrix,
+        # Set a 1-D condensed distance amtrix.
+        self.set_distance_matrix(index)
+        
+        # Set the linkage matrix as a result of the agglomerative hierarchical clustering process.
+        self.linked = linkage(self.proximity_matrix,
                          method=method,
                          optimal_ordering=True)
-        # Menggambar dendrogram berdasarkan hasil hierarchical clustering.
-        fig = plt.figure(figsize=(10, 5))
-        dend = dendrogram(linked,
+        
+        dend = dendrogram(self.linked,
                     orientation='right',
                     color_threshold=cut_off,
-                    labels=[doc.get_title() for doc in corpus])
-        # Menggambarkan garis pemotong (cut-off).
-        plt.axvline(x=cut_off, linestyle='dashed')
-        # Mengatur agar plot tidak terpotong.
-        plt.tight_layout()
-        # Menyimpan daftar cluster beserta anggota-anggotanya.
-        self.set_cluster(dend)
-        # Menyimpan tinggi maksimal dari dendrogram yang dibangun.
-        self.set_dendrogram_height(dend)
-        # Menyimpan hasil evaluasi clustering dengan CPCC.
-        self.set_cophenet_coeff(proximity_matrix, linked)
-        return fig
+                    labels=[doc.get_title() for doc in self.corpus])
+        self.dendrogram = Dendrogram(dend)
+        return self.dendrogram.plot_dendrogram()
---
+++ src/clustering/Dendrogram.py
@@ -0,0 +1,52 @@
+
+import matplotlib.pyplot as plt
+
+class Dendrogram:
+    
+    def __init__(self, dend):
+        self.dend = dend
+        
+    def plot_dendrogram(self, cut_off=0):
+        """
+        Plot dendrogram diagram
+
+        Parameters
+        ----------
+        cut_off : float, optional
+            The height of a cut-off point. The default is 0.
+
+        Returns
+        -------
+        fig : figure
+            A figure of the dendrogram.
+
+        """
+        fig = plt.figure(figsize=(10, 5))
+        dendrogram = self.dend
+        
+        # Cut the dendrogram at cut_off height.
+        # If the cut-off height equals to 0, there will be no cut-off line.
+        plt.axvline(x=cut_off, linestyle='dashed')
+        return fig
+    
+    def extract_clusters_by_color(self):
+        """
+        Extract a list of objects of each clusters in a dendrogram based on colors.
+        Shout out to http://www.nxn.se/valent/extract-cluster-elements-by-color-in-python.
+        
+        Returns
+        -------
+        cluster_list : dictionary
+            A list of objects of each clusters.
+            Written as {cluster1: [doc1, doc2], cluster2: [doc3], etc.}.
+
+        """
+        cluster_list = {}
+        for c, pi in zip(self.dend['color_list'], self.dend['icoord']):
+            for leg in pi[1:3]:
+                i = (leg - 5) / 10
+                if abs(i - int(i)) <= 0:
+                    if c not in self.cluster_list:
+                        cluster_list[c] = []
+                    cluster_list[c].append(self.dend['ivl'][int(i)])
+        return cluster_list
