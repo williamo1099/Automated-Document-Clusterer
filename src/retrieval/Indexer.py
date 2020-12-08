0edc7aa9a63efc53624e037f9f87c9508f2538b4
William Oktavianus <williamoktavianus1099@gmail.com>
2020-12-08 18:46:08 +0700

    Perbaikan kelas Indexer
    Memperbaiki kelas Indexer dan mengganti kelas Tokenizer dan LinguisticPreprocesser sebagai inner class dalam Indexer (karena hanya dibutuhkan dalam kelas Indexer).

--- src/retrieval/Indexer.py
+++ src/retrieval/Indexer.py
@@ -1,81 +1,175 @@
 
-from retrieval.Tokenizer import Tokenizer
-from retrieval.LinguisticPreprocesser import LinguisticPreprocesser
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 class Indexer:
     
     def __init__(self):
-        self.inverted_index = {}
-        
-    def get_inverted_index(self):
         """
-        Method untuk mendapatkan nilai atribut inverted_index, yang merupakan indeks yang telah dibangun.
-        Inverted index menyimpan pemetaan term ke lokasi term tersebut berada.
-
-        Returns
-        -------
-        inverted_index : dictionary
-            Inverted index.
-            Ditulis sebagai {term1: {doc1: freq, doc2: freq}, term2: {doc1: freq}}.
-
-        """
-        return self.inverted_index
-    
-    def preprocess(self, document):
-        """
-        Method untuk mengambil daftar term dari dokumen teks.
-        Proses yang dilakukan adalah proses tokenisasi dan pre-pemrosesan linguistik.
-
-        Parameters
-        ----------
-        document : Document
-            Dokumen teks.
-
-        Returns
-        -------
-        dictionary : list
-            Daftar term yang ada dalam dokumen teks.
-
-        """
-        # Melakukan proses tokenisasi.
-        tokenizer = Tokenizer()
-        token_list = tokenizer.tokenize(document.get_content())
-        # Melakukan pre-pemrosesan linguistik (stop words removal, stemming).
-        preprocesser = LinguisticPreprocesser()
-        dictionary = preprocesser.preprocess(token_list)
-        return dictionary
-    
-    def index(self, document):
-        """
-        Method untuk membangun inverted index dan menyimpannya sebagai nilai atribut inverted_index.
-        Pembangunan inverted index dilakukan untuk sebuah dokumen teks (satu per satu).
-        Inverted index yang dibangun ditulis sebagai {term1: {doc1: freq, doc2: freq}, term2: {doc1: freq}}.
-
-        Parameters
-        ----------
-        document : Document
-            Dokumen teks.
+        The constructor for Indexer class.
 
         Returns
         -------
         None.
 
         """
-        # Mendapatkan daftar term dari dokumen teks.
+        self.inverted_index = {}
+        
+    class Tokenizer:
+        
+        @staticmethod
+        def tokenize(sequence):
+            """
+            The method to tokenize a character sequence into a list of tokens.
+
+            Parameters
+            ----------
+            sequence : string
+                The character sequence to be tokenized.
+
+            Returns
+            -------
+            result : list
+                The list of tokens.
+
+            """
+            # Converting all uppercase characters into lowercase characters (case folding).
+            tokens = word_tokenize(sequence.lower())
+            result = []
+            for token in tokens:
+                
+                # Check whether a token contains any non-alphabet character.
+                # All non-alphabet character in a token will be removed.
+                if token.isalpha():
+                    result.append(token)
+                else:
+                    normalized = ''.join(char for char in token if char.isalpha())
+                    if normalized != '':
+                        result.append(normalized)
+            return result
+        
+    class LinguisticPreprocesser:
+        
+        @staticmethod
+        def remove_stopwords(token_list):
+            """
+            The method to remove stop words from a list of tokens.
+            Language supported is English.
+
+            Parameters
+            ----------
+            token_list : list
+                The list of tokens.
+
+            Returns
+            -------
+            result : list
+                The list of terms (list of tokens without stop words).
+
+            """
+            stop_list = stopwords.words('english')
+            result = []
+            for token in token_list:
+                
+                # Check whether a token is a stop word or not.
+                # If a token is a stop word, the token will be removed.
+                if token not in stop_list:
+                    result.append(token)
+            return result
+    
+        @staticmethod
+        def stem(token_list):
+            """
+            The method to stem all tokens in a list of tokens.
+            Technique used is suffix-stripping technique, with Porter stemming algorithm.
+            Language supported is English.
+
+            Parameters
+            ----------
+            token_list : list
+                The list of tokens.
+
+            Returns
+            -------
+            result : list
+                The list of terms (list of stemmed tokens).
+
+            """
+            stemmer = PorterStemmer()
+            result = []
+            for token in token_list:
+                stemmed = stemmer.stem(token)
+                result.append(stemmed)
+            return result
+    
+    def get_inverted_index(self):
+        """
+        The method to get inverted index built by method index.
+
+        Returns
+        -------
+        dictionary
+            The inverted index built.
+            Written as {term1: {doc1: freq, doc2: freq}, term2: {doc1: freq}, etc.}.
+
+        """
+        return self.inverted_index
+    
+    def preprocess(self, document):
+        """
+        The method to get a list of terms in a document.
+        Each term is a result of linguistic preprocessing of each tokens in the document.
+
+        Parameters
+        ----------
+        document : Document
+            The document.
+
+        Returns
+        -------
+        dictionary : list
+            The list of terms in the document.
+
+        """
+        # Do tokenization process.
+        token_list = self.Tokenizer.tokenize(document.get_content())
+        
+        # Do linguistic preprocessing (stop words removal and stemming).
+        dictionary = self.LinguisticPreprocesser.stem(
+                    self.LinguisticPreprocesser.stem(token_list))
+        return dictionary
+    
+    def index(self, document):
+        """
+        The method to build an inverted index.
+
+        Parameters
+        ----------
+        document : Document
+            The document.
+
+        Returns
+        -------
+        None.
+
+        """
+        # Get the list of terms in a document.
         dictionary = self.preprocess(document)
-        # Mengambil id dari dokumen teks.
+        
+        # Get the document's id.
         doc_id = document.get_id()
         for term in dictionary:
-            # Melihat apakah term ada dalam inverted index.
+            
+            # Check whether a term is already stored in the index.
+            # If a term is not stored yet, the term will be stored as a new key in the index.
             if term not in self.inverted_index:
-                # Jika term belum ada, dibuat key baru dalam index.
-                # Sebuah key berisi dictionary kosong, untuk menyimpan dokumen teks dan frekuensinya.
-                # Contohnya, {doc1 : 1, doc2 : 3}.
                 self.inverted_index[term] = {}
-            # Melihat apakah sebuah dokumen teks sudah tercatat dalam daftar posting index.
+                
+            # Check whether the document is stored in the posting list.
+            # If the document is not stored yet, the frequency will be set to 1.
+            # If the document is stored already, the frequency will be set to the previous frequency + 1.
             if doc_id not in self.inverted_index[term]:
-                # Jika belum ada, init frekuensinya 1.
                 self.inverted_index[term][doc_id] = 1
             else:
-                # Jika sudah ada, frekuensinya ditambah sebesar 1.
                 self.inverted_index[term][doc_id] += 1
