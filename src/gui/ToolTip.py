a894aa77a52c21daf62673e1f372d04748d65214
William Oktavianus <williamoktavianus1099@gmail.com>
2021-03-17 15:26:02 +0700

    Perbaikan minor
    Memperbaiki penggunaan public variabel dari ToolTip.

--- src/gui/ToolTip.py
+++ src/gui/ToolTip.py
@@ -19,10 +19,10 @@ class ToolTip:
         None.
 
         """
-        self.widget = widget
-        self.widget.bind('<Enter>', self.__enter)
-        self.widget.bind('<Leave>', self.__close)
-        self.tip = tip
+        self.__widget = widget
+        self.__widget.bind('<Enter>', self.__enter)
+        self.__widget.bind('<Leave>', self.__close)
+        self.__tip = tip
         
     def __enter(self, event=None):
         """
@@ -39,14 +39,14 @@ class ToolTip:
 
         """
         x = y = 0
-        x, y, cx, cy = self.widget.bbox('insert')
-        x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() - 2
+        x, y, cx, cy = self.__widget.bbox('insert')
+        x += self.__widget.winfo_rootx() + 25
+        y += self.__widget.winfo_rooty() - 2
         
-        self.tool_tip = tk.Toplevel(self.widget)
+        self.tool_tip = tk.Toplevel(self.__widget)
         self.tool_tip.wm_overrideredirect(True)
         self.tool_tip.wm_geometry('+%d+%d' % (x, y))
-        label = tk.Label(self.tool_tip, text=self.tip, borderwidth=1, relief='solid')
+        label = tk.Label(self.tool_tip, text=self.__tip, borderwidth=1, relief='solid')
         label.pack(ipadx=5)
         
     def __close(self, event=None):
