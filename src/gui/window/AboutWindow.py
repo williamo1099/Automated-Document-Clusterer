f4163537a45c71619bd812253639f1bc251a42e6
William Oktavianus <williamoktavianus1099@gmail.com>
2021-04-19 14:12:21 +0700

    Perbaikan minor
    Memperbaiki struktur AboutWindow (minor).

--- src/gui/window/AboutWindow.py
+++ src/gui/window/AboutWindow.py
@@ -29,28 +29,28 @@ class AboutWindow:
         self.__window.resizable(width=False, height=False)
         
         # Initialize the logo in the window.
-        app_logo = tk.PhotoImage(file='resources/logo100.png', width=100, height=100)
-        panel = tk.Label(master=self.__window, image=app_logo)
-        panel.pack(side='top', pady=10)
+        self.__app_logo = tk.PhotoImage(file='resources/logo100.png', width=100, height=100)
+        self.__panel = tk.Label(master=self.__window, image=self.__app_logo)
+        self.__panel.pack(side='top', pady=10)
         
         # Initialize the title label in the window.
-        title_label = tk.Label(master=self.__window, text='Automated Document Clusterer')
-        title_label.configure(font=("bold"))
-        title_label.pack(side='top', pady=1)
+        self.__title_label = tk.Label(master=self.__window, text='Automated Document Clusterer')
+        self.__title_label.configure(font=("bold"))
+        self.__title_label.pack(side='top', pady=1)
         
         # Initialize the version label in the window.
-        version_label = tk.Label(master=self.__window, text='Version 0.0.0')
-        version_label.pack(side='top', pady=1)
+        self.__version_label = tk.Label(master=self.__window, text='Version 0.0.0')
+        self.__version_label.pack(side='top', pady=1)
         
         # Initialize the dependency label in the window.
-        dependency_label = tk.Label(master=self.__window, text='Built with\n' +
+        self.__dependency_label = tk.Label(master=self.__window, text='Built with\n' +
                                     'Matplotlib 3.3.2\n' + 'NLTK 3.5\n' + 'SciPy 1.5.4')
-        dependency_label.pack(side='top', pady=2)
+        self.__dependency_label.pack(side='top', pady=2)
         
         # Initialize the version label in the window.
-        copyright_label = tk.Label(master=self.__window, text='Copyright (c) 2020 HotSpace')
-        copyright_label.configure(fg='#808080')
-        copyright_label.pack(side='bottom', pady=10)
+        self.__copyright_label = tk.Label(master=self.__window, text='Copyright (c) 2020')
+        self.__copyright_label.configure(fg='#808080')
+        self.__copyright_label.pack(side='bottom', pady=10)
     
     def _start(self):
         """
